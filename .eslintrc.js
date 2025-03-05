5a013721502951ec8d38bab2ea3c270f876967ac
malikk2 <malikk2@lancaster.ac.uk>
2025-03-05 22:39:12 +0400

    Relax ESLint rules to reduce warnings and add Next.js font rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
     extends: 'next',
     rules: {
-        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-unused-vars': 'off',
         'react/no-unescaped-entities': 'off',
-        'react-hooks/exhaustive-deps': 'warn',
-        '@typescript-eslint/no-explicit-any': 'warn',
-        'prefer-const': 'warn',
-        '@typescript-eslint/no-unused-expressions': 'warn'
+        'react-hooks/exhaustive-deps': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'prefer-const': 'off',
+        '@typescript-eslint/no-unused-expressions': 'off',
+        '@next/next/no-page-custom-font': 'off',
     }
 } 
