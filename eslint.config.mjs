fb13800b310cfce80d6861ef6b39a8d85456c24b
malikk2 <malikk2@lancaster.ac.uk>
2025-03-05 22:41:09 +0400

    Disable additional ESLint rules to reduce development warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,18 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'react/no-unescaped-entities': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'prefer-const': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
